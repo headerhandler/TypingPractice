35eca060a41bd09094a6d0dd155d6594c379ed8f
oduaht <oduaht@gmail.com>
2013-10-18 20:49:45 +0100

    add source files

---
+++ PlatformTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author OLUWATOBI ODUAH
+ */
+public class PlatformTest {
+    public static void main(String[] args)
+    {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TypePlatform.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TypePlatform.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TypePlatform.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TypePlatform.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        TypePlatform pl = new TypePlatform();
+        pl.setVisible(true);
+    }
+}
